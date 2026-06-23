259238d636beccff069a000addee8d43f15de8ef
Furinmatcha <matchafurin@gmail.com>
2026-06-23 14:39:59 +0700

    Update driveHandler.js

--- handlers/driveHandler.js
+++ handlers/driveHandler.js
@@ -1,39 +1,6 @@
-const { google } = require('googleapis')
-const { Readable } = require('stream')
-
-const auth = new google.auth.GoogleAuth({
-  credentials: JSON.parse(process.env.GOOGLE_CREDENTIALS),
-  scopes: ['https://www.googleapis.com/auth/drive']
-})
-
 async function uploadFile(buffer, filename) {
-  const client = await auth.getClient()
-  const drive = google.drive({ version: 'v3', auth: client })
-
-  const stream = Readable.from(buffer)
-
-  const res = await drive.files.create({
-    requestBody: {
-      name: filename,
-      parents: [process.env.GOOGLE_DRIVE_FOLDER_ID]
-    },
-    media: {
-      mimeType: 'image/jpeg',
-      body: stream
-    },
-    fields: 'id, webViewLink'
-  })
-
-  // ตั้งให้ดูได้โดยไม่ต้อง login
-  await drive.permissions.create({
-    fileId: res.data.id,
-    requestBody: {
-      role: 'reader',
-      type: 'anyone'
-    }
-  })
-
-  return res.data.webViewLink
+  // TODO: fix Drive upload later
+  return '-'
 }
 
 module.exports = { uploadFile }
