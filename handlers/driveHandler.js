681d0111781728c08f85bfdcc4b70d422193a6b7
Furinmatcha <matchafurin@gmail.com>
2026-06-24 12:01:19 +0700

    Update driveHandler.js

--- handlers/driveHandler.js
+++ handlers/driveHandler.js
@@ -1,72 +1,72 @@
 const { google } = require('googleapis');
 const { Readable } = require('stream');
 
-const credentials = JSON.parse(process.env.GOOGLE_CREDENTIALS);
-const auth = new google.auth.GoogleAuth({
- credentials,
- scopes: ['https://www.googleapis.com/auth/drive'],
+const oauth2Client = new google.auth.OAuth2(
+  process.env.GOOGLE_OAUTH_CLIENT_ID,
+  process.env.GOOGLE_OAUTH_CLIENT_SECRET,
+  'https://developers.google.com/oauthplayground'
+);
+
+oauth2Client.setCredentials({
+  refresh_token: process.env.GOOGLE_OAUTH_REFRESH_TOKEN,
 });
-const drive = google.drive({ version: 'v3', auth });
+
+const drive = google.drive({ version: 'v3', auth: oauth2Client });
 
 const THAI_MONTHS = [
- 'มกราคม','กุมภาพันธ์','มนาคม','เมษายน','พฤษภาคม','มิถุนายน',
- 'กรกฎาคม','สิงหาคม','กันยายน','ตุลาคม','พฤศจิกายน','ธันวาคม'
+  'มกราคม','กุมภาพันธ์','มีนาคม','เมษายน','พฤษภาคม','มิถุนายน',
+  'กรกฎาคม','สิงหาคม','กันยายน','ตุลาคม','พฤศจิกายน','ธันวาคม'
 ];
 
 async function findOrCreateFolder(name, parentId) {
- const q = `name='${name}' and mimeType='application/vnd.google-apps.folder' `
-         + `and '${parentId}' in parents and trashed=false`;
+  const q = `name='${name}' and mimeType='application/vnd.google-apps.folder' `
+          + `and '${parentId}' in parents and trashed=false`;
 
- const res = await drive.files.list({
-   q,
-   fields: 'files(id, name)',
-   spaces: 'drive',
-   supportsAllDrives: true,
-   includeItemsFromAllDrives: true,
- });
+  const res = await drive.files.list({
+    q,
+    fields: 'files(id, name)',
+    spaces: 'drive',
+  });
 
- if (res.data.files.length > 0) return res.data.files[0].id;
+  if (res.data.files.length > 0) return res.data.files[0].id;
 
- const folder = await drive.files.create({
-   requestBody: {
-     name,
-     mimeType: 'application/vnd.google-apps.folder',
-     parents: [parentId],
-   },
-   fields: 'id',
-   supportsAllDrives: true,
- });
- return folder.data.id;
+  const folder = await drive.files.create({
+    requestBody: {
+      name,
+      mimeType: 'application/vnd.google-apps.folder',
+      parents: [parentId],
+    },
+    fields: 'id',
+  });
+  return folder.data.id;
 }
 
 async function buildFolderPath(dateStr, payee, txnId) {
- const [day, month, year] = dateStr.split('/');
- const monthFolder = `${month.padStart(2,'0')}_${THAI_MONTHS[parseInt(month)-1]}`;
- const txnFolderName = `${year}-${month.padStart(2,'0')}-${day.padStart(2,'0')}_${payee}_${txnId}`;
+  const [day, month, year] = dateStr.split('/');
+  const monthFolder = `${month.padStart(2,'0')}_${THAI_MONTHS[parseInt(month)-1]}`;
+  const txnFolderName = `${year}-${month.padStart(2,'0')}-${day.padStart(2,'0')}_${payee}_${txnId}`;
 
- const ROOT = process.env.GOOGLE_DRIVE_FOLDER_ID;
- const yearId = await findOrCreateFolder(year, ROOT);
- const monthId = await findOrCreateFolder(monthFolder, yearId);
- const evidenceRoot = await findOrCreateFolder('รวมหลักฐาน', monthId);
- const accountingRoot = await findOrCreateFolder('สำหรับสำนักงานบัญชี', monthId);
- const txnFolderId = await findOrCreateFolder(txnFolderName, evidenceRoot);
+  const ROOT = process.env.GOOGLE_DRIVE_FOLDER_ID;
+  const yearId = await findOrCreateFolder(year, ROOT);
+  const monthId = await findOrCreateFolder(monthFolder, yearId);
+  const evidenceRoot = await findOrCreateFolder('รวมหลักฐาน', monthId);
+  const accountingRoot = await findOrCreateFolder('สำหรับสำนักงานบัญชี', monthId);
+  const txnFolderId = await findOrCreateFolder(txnFolderName, evidenceRoot);
 
- return { txnFolderId, accountingRoot };
+  return { txnFolderId, accountingRoot };
 }
 
 async function uploadToDrive(buffer, fileName, parentId, mimeType = 'image/jpeg') {
- const res = await drive.files.create({
-   requestBody: { name: fileName, parents: [parentId] },
-   media: { mimeType, body: Readable.from(buffer) },
-   fields: 'id, webViewLink',
-   supportsAllDrives: true,
- });
- await drive.permissions.create({
-   fileId: res.data.id,
-   requestBody: { role: 'reader', type: 'anyone' },
-   supportsAllDrives: true,
- });
- return res.data.webViewLink;
+  const res = await drive.files.create({
+    requestBody: { name: fileName, parents: [parentId] },
+    media: { mimeType, body: Readable.from(buffer) },
+    fields: 'id, webViewLink',
+  });
+  await drive.permissions.create({
+    fileId: res.data.id,
+    requestBody: { role: 'reader', type: 'anyone' },
+  });
+  return res.data.webViewLink;
 }
 
 module.exports = { buildFolderPath, uploadToDrive };
